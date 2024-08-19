eb4091def8d33f07e9caf6d80f73b53aa11d642b
Rafalamao <aaronjt@outlook.com>
2024-08-18 18:10:41 -0600

    primer video xd

---
+++ 1_PrimerosPasos/src/PrimeraClase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * https://www.youtube.com/watch?v=PT8AziBfrdQ&list=PLU8oAlHdN5BktAXdEVCLUYzvDyqRQJ2lk&index=4
+ */
+
+/**
+ *
+ * @author rafalamao
+ */
+public class PrimeraClase {
+    public static void main(String []args)
+    {
+        System.out.println("Hola alumnos.");
+    }
+}
